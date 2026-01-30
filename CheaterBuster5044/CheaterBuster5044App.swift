ba2ba545cb0770019c0f1893ec4c42bd96e67103
Alexander <158753527+Pechinkin04@users.noreply.github.com>
2026-01-30 18:11:17 +0400

    Initial Commit

---
+++ CheaterBuster5044/CheaterBuster5044App.swift
@@ -0,0 +1,17 @@
+//
+//  CheaterBuster5044App.swift
+//  CheaterBuster5044
+//
+//  Created by Александр Печинкин on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CheaterBuster5044App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
